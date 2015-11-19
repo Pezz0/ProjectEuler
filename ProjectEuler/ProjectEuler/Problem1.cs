bfa7f89d7ec65029d6ac4f8b121ad1e159a2c3c7
Pezzo <pezzo92@gmail.com>
2015-11-19 18:56:26 +0100

    Fisrt couple problems

---
+++ ProjectEuler/ProjectEuler/Problem1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+    class Problem1
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            for (int i = 3;  i < 1000;i++) {
+                if (i % 3 == 0 || i % 5 == 0) sum = sum + i;
+            }
+            Console.WriteLine(sum);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ProjectEuler/[2] Even Fibonacci numbers/Problem2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2__Even_Fibonacci_numbers
+{
+    class Problem2
+    {
+        static void Main(string[] args)
+        {
+            int sum = 2;
+            int prev_1 = 2;
+            int prev_2 = 1;
+            int now = 3;
+            while (now<4000000)
+            {
+                if (now % 2 == 0) sum += now;
+                prev_2 = prev_1;
+                prev_1 = now;
+                now = prev_1 + prev_2;   
+            }
+            Console.WriteLine(sum);
+            Console.ReadLine();
+        }
+    }
+}
